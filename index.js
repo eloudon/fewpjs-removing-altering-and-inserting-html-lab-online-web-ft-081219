eb1613c3f1afe9eaeff5af123cbbf7af6e1f9b90
Eric Loudon <loudon.eric@gmail.com>
2019-11-28 18:58:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ document.querySelector("main").remove();
 let newHeader = document.createElement('h1');
 document.body.appendChild(newHeader);
 newHeader.id = "victory"
-newHeader.innerText = "Eric is the champion" 
+newHeader.innerText = "Eric is the champion!"
